fec691fa13090145e2630d22980ebb38b1f249f3
donnicopalma <nico.palma@gmail.com>
2015-07-08 21:39:09 -0300

    Base de datos
    Tablas agregadas y nuevas relaciones

--- roomb_users.sql
+++ roomb_users.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 3.4.5
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 08, 2015 at 08:04 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Servidor: localhost
+-- Tiempo de generación: 09-07-2015 a las 02:37:52
+-- Versión del servidor: 5.5.16
+-- Versión de PHP: 5.3.8
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,71 +17,127 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `roomb_users`
+-- Base de datos: `roomb_users`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `familia_objetos`
+-- Estructura de tabla para la tabla `amigo`
+--
+
+CREATE TABLE IF NOT EXISTS `amigo` (
+  `id_user_1` int(11) DEFAULT NULL,
+  `id_user_2` int(11) DEFAULT NULL,
+  `id_tipo` int(11) DEFAULT NULL,
+  KEY `id_user_1` (`id_user_1`,`id_user_2`,`id_tipo`),
+  KEY `id_tipo` (`id_tipo`),
+  KEY `id_user_2` (`id_user_2`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuarto`
+--
+
+CREATE TABLE IF NOT EXISTS `cuarto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `contador_visitas` int(11) NOT NULL,
+  `alto` int(11) NOT NULL,
+  `largo` int(11) NOT NULL,
+  `ancho` int(11) NOT NULL,
+  `luces` int(1) NOT NULL,
+  `color_luces` int(2) NOT NULL,
+  `color` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `familia_objetos`
 --
 
 CREATE TABLE IF NOT EXISTS `familia_objetos` (
-`id` int(11) NOT NULL,
-  `nombre` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `imagenes`
+-- Estructura de tabla para la tabla `imagenes`
 --
 
 CREATE TABLE IF NOT EXISTS `imagenes` (
-`id_user` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `foto_perfil` varchar(100) NOT NULL,
   `miniatura_perfil` varchar(100) NOT NULL,
   `directorio` varchar(100) NOT NULL,
-  `imagen_cuarto` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `imagen_cuarto` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `objeto`
+-- Estructura de tabla para la tabla `objeto`
 --
 
 CREATE TABLE IF NOT EXISTS `objeto` (
-`id` int(11) NOT NULL,
-  `id_user` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cuarto` int(11) DEFAULT NULL,
   `id_tipo_objeto` int(11) DEFAULT NULL,
-  `tamano` int(4) DEFAULT NULL,
+  `alto` int(4) DEFAULT NULL,
+  `ancho` int(4) NOT NULL,
   `posicion_z` int(5) DEFAULT NULL,
-  `posicion_x` int(5) DEFAULT NULL
+  `posicion_x` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_tipo_objeto` (`id_tipo_objeto`),
+  KEY `id_cuarto` (`id_cuarto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_amigo`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_amigo` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `nombre` varchar(15) DEFAULT NULL,
+  `perimso` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipo_objeto`
+-- Estructura de tabla para la tabla `tipo_objeto`
 --
 
 CREATE TABLE IF NOT EXISTS `tipo_objeto` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_familia` int(11) DEFAULT NULL,
   `nombre` varchar(20) DEFAULT NULL,
   `marca` varchar(20) DEFAULT NULL,
-  `modelo` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `modelo` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_familia` (`id_familia`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Estructura de tabla para la tabla `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(20) DEFAULT NULL,
   `apellidos` varchar(40) DEFAULT NULL,
   `contrasena` varchar(30) DEFAULT NULL,
@@ -89,11 +145,12 @@ CREATE TABLE IF NOT EXISTS `user` (
   `sexo` tinyint(1) DEFAULT NULL,
   `fecha_nac` date DEFAULT NULL,
   `fecha_registro` date DEFAULT NULL,
-  `dinero` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `dinero` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `user`
+-- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`ID`, `nombre`, `apellidos`, `contrasena`, `mail`, `sexo`, `fecha_nac`, `fecha_registro`, `dinero`) VALUES
@@ -102,11 +159,11 @@ INSERT INTO `user` (`ID`, `nombre`, `apellidos`, `contrasena`, `mail`, `sexo`, `
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-`id` int(4) NOT NULL,
+  `id` int(4) NOT NULL AUTO_INCREMENT,
   `pass` varchar(255) NOT NULL,
   `user` varchar(255) NOT NULL,
   `foto` varchar(255) NOT NULL,
@@ -123,112 +180,53 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `nivel` int(4) NOT NULL,
   `fecha_registro` varchar(255) NOT NULL,
   `hora_registro` varchar(255) NOT NULL,
-  `IP` tinytext NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `IP` tinytext NOT NULL,
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `pass`, `user`, `foto`, `fotoB`, `nombre`, `apellidos`, `fecha_nac`, `ocup`, `ocup_s`, `pais`, `region`, `sexo`, `zipcode`, `nivel`, `fecha_registro`, `hora_registro`, `IP`) VALUES
 (1, '1234', 'nico', 'nicolas/new2.png', 'nicolas/new.png', 'nico', 'palma', '', '', '', '', '', 'm', '', 1, '', '', '');
 
 --
--- Indexes for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Indexes for table `familia_objetos`
+-- Filtros para la tabla `amigo`
 --
-ALTER TABLE `familia_objetos`
- ADD PRIMARY KEY (`id`);
+ALTER TABLE `amigo`
+  ADD CONSTRAINT `amigo_ibfk_3` FOREIGN KEY (`id_user_2`) REFERENCES `user` (`ID`),
+  ADD CONSTRAINT `amigo_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipo_amigo` (`id`),
+  ADD CONSTRAINT `amigo_ibfk_2` FOREIGN KEY (`id_user_1`) REFERENCES `user` (`ID`);
 
 --
--- Indexes for table `imagenes`
+-- Filtros para la tabla `cuarto`
+--
+ALTER TABLE `cuarto`
+  ADD CONSTRAINT `cuarto_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`ID`),
+  ADD CONSTRAINT `cuarto_ibfk_2` FOREIGN KEY (`id`) REFERENCES `objeto` (`id_cuarto`);
+
+--
+-- Filtros para la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
- ADD PRIMARY KEY (`id_user`);
+  ADD CONSTRAINT `imagenes_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`ID`);
 
 --
--- Indexes for table `objeto`
+-- Filtros para la tabla `objeto`
 --
 ALTER TABLE `objeto`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_tipo_objeto` (`id_tipo_objeto`), ADD UNIQUE KEY `id_user` (`id_user`);
+  ADD CONSTRAINT `objeto_ibfk_2` FOREIGN KEY (`id_tipo_objeto`) REFERENCES `tipo_objeto` (`id`);
 
 --
--- Indexes for table `tipo_objeto`
+-- Filtros para la tabla `tipo_objeto`
 --
 ALTER TABLE `tipo_objeto`
- ADD PRIMARY KEY (`id`), ADD KEY `id_familia` (`id_familia`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
- ADD KEY `id` (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `familia_objetos`
---
-ALTER TABLE `familia_objetos`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `imagenes`
---
-ALTER TABLE `imagenes`
-MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `objeto`
---
-ALTER TABLE `objeto`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tipo_objeto`
---
-ALTER TABLE `tipo_objeto`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-MODIFY `id` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `imagenes`
---
-ALTER TABLE `imagenes`
-ADD CONSTRAINT `imagenes_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`ID`);
-
---
--- Constraints for table `objeto`
---
-ALTER TABLE `objeto`
-ADD CONSTRAINT `objeto_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`ID`),
-ADD CONSTRAINT `objeto_ibfk_2` FOREIGN KEY (`id_tipo_objeto`) REFERENCES `tipo_objeto` (`id`);
-
---
--- Constraints for table `tipo_objeto`
---
-ALTER TABLE `tipo_objeto`
-ADD CONSTRAINT `tipo_objeto_ibfk_1` FOREIGN KEY (`id_familia`) REFERENCES `familia_objetos` (`id`);
+  ADD CONSTRAINT `tipo_objeto_ibfk_1` FOREIGN KEY (`id_familia`) REFERENCES `familia_objetos` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
