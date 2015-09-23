65c3cd84544a53c2ceb017e81b0397fda580a156
donnicopalma <nico.palma@gmail.com>
2015-09-22 21:43:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	
 	session_start();
 ?>
-Se crean carpetas, se eliminan archivos y se mueven archivos a las carpetas creadas para dar orden.
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
